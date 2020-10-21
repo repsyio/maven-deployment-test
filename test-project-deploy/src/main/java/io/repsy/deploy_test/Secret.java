00853b11b887d59f18b7f0373a4f0a4b84cb28c8
firatkucuk <firat@fria.io>
2020-10-21 20:53:01 +0200

    secret updated

--- test-project-deploy/src/main/java/io/repsy/deploy_test/Secret.java
+++ test-project-deploy/src/main/java/io/repsy/deploy_test/Secret.java
@@ -3,6 +3,6 @@ package io.repsy.deploy_test;
 public class Secret {
 
     public static void printSecret() {
-        System.out.println("ODhiwUB0iLJTW5mJ");
+        System.out.println("XXXX");
     }
 }

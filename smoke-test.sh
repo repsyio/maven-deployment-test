68353c62c3448e61f364081ff2cc3b3bef063a10
firatkucuk <firat@fria.io>
2021-01-09 05:59:58 +0100

    smoke test script updated

--- smoke-test.sh
+++ smoke-test.sh
@@ -6,34 +6,30 @@ BASE_DIR=$(dirname "$0")
 
 cd "$BASE_DIR"
 
-if [ -z "$REPSY_BASE_URL" ]
-then
-    export REPSY_BASE_URL="https://repo.repsy.io"
+if [ -z "$REPSY_BASE_URL" ]; then
+  export REPSY_BASE_URL="https://repo.repsy.io"
 fi
 
 export REPSY_USERNAME="$1"
 export REPSY_REPO_NAME="$2"
 export REPSY_REPO_PASSWORD="$3"
 
-if [ "$#" != 3 ];
-then
-    echo 'usage: ./smoke-test.sh username repo_name repo_password'
-    exit 1
+if [ "$#" != 3 ]; then
+  echo 'usage: ./smoke-test.sh username repo_name repo_password'
+  exit 1
 fi
-
 
 EXEC_VALUE=""
 
-save_maven_settings () {
+save_maven_settings() {
 
-    mkdir -p ~/.m2
+  mkdir -p ~/.m2
 
-    if [ -f ~/.m2/settings.xml ]
-    then
-        cp ~/.m2/settings.xml ~/.m2/settings.xml.repsy_backup
-    fi
+  if [ -f ~/.m2/settings.xml ]; then
+    cp ~/.m2/settings.xml ~/.m2/settings.xml.repsy_backup
+  fi
 
-    cat <<EOF > ~/.m2/settings.xml
+  cat <<EOF >~/.m2/settings.xml
 <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
     xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
@@ -50,17 +46,19 @@ save_maven_settings () {
 EOF
 }
 
-rollback_maven_settings () {
-    if [ -f ~/.m2/settings.xml.repsy_backup ]
-    then
-        cp ~/.m2/settings.xml.repsy_backup ~/.m2/settings.xml
-    fi
+rollback_maven_settings() {
+  if [ -f ~/.m2/settings.xml.repsy_backup ]; then
+    cp ~/.m2/settings.xml.repsy_backup ~/.m2/settings.xml
+  fi
 }
 
-deploy () {
-    TEST_VALUE="$1"
+deploy() {
+  TEST_VALUE="$1"
 
-cat <<EOF > test-project-deploy/src/main/java/io/repsy/deploy_test/Secret.java
+  cd test-project-deploy
+  mv src/main/java/io/repsy/deploy_test/Secret.java \
+    src/main/java/io/repsy/deploy_test/Secret.java.backup
+  cat <<EOF >src/main/java/io/repsy/deploy_test/Secret.java
 package io.repsy.deploy_test;
 
 public class Secret {
@@ -70,19 +68,21 @@ public class Secret {
     }
 }
 EOF
+  ./mvnw deploy
 
-    cd test-project-deploy
-    ./mvnw deploy
-    cd ../
+  mv src/main/java/io/repsy/deploy_test/Secret.java.backup \
+    src/main/java/io/repsy/deploy_test/Secret.java
+
+  cd ../
 }
 
-download_and_test () {
+download_and_test() {
 
-    cd test-project-download
-    ./mvnw compile dependency:copy-dependencies
-    cd target/classes
-    EXEC_VALUE=$(java -cp ../dependency/deploy-test-1.0.jar:. io.repsy.download_test.Main)
-    cd ../../../
+  cd test-project-download
+  ./mvnw compile dependency:copy-dependencies
+  cd target/classes
+  EXEC_VALUE=$(java -cp ../dependency/deploy-test-1.0.jar:. io.repsy.download_test.Main)
+  cd ../../../
 }
 
 save_maven_settings
@@ -90,18 +90,17 @@ RANDOM_VALUE=$(openssl rand -base64 12)
 deploy "$RANDOM_VALUE"
 download_and_test
 
-if [ "$RANDOM_VALUE" != "$EXEC_VALUE" ];
-then
-    exit 1
+if [ "$RANDOM_VALUE" != "$EXEC_VALUE" ]; then
+  exit 1
 fi
 
+# Redeploy
 RANDOM_VALUE=$(openssl rand -base64 12)
 deploy "$RANDOM_VALUE"
 download_and_test
 
-if [ "$RANDOM_VALUE" != "$EXEC_VALUE" ];
-then
-    exit 1
+if [ "$RANDOM_VALUE" != "$EXEC_VALUE" ]; then
+  exit 1
 fi
 
 rollback_maven_settings
